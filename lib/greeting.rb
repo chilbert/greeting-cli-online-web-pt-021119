c2f4ba2a412c35ee7c2f44ee163dfb04e353c277
Chase Hilbert <chase@heyspacetime.com>
2019-02-12 21:15:48 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
 def greeting(name)
   name_space = name.chomp
-  puts "Hello #{name}. It's nice to meet you."
+  puts "Hello #{name_space}. It's nice to meet you."
 end
